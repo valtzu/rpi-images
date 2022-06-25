8ee814d2beac0c6db3c26afd7353a62e001a1cb1
valtzu <valtzu@gmail.com>
2022-06-25 15:02:47 +0300

    Upgrade to newer kernel

--- ubuntu-kernel.sh
+++ ubuntu-kernel.sh
@@ -9,7 +9,7 @@ apt-get update
 rm -f /etc/kernel/postinst.d/initramfs-tools /etc/kernel/postinst.d/zz-flash-kernel /etc/initramfs/post-update.d/flash-kernel
 
 # Make sure required tools are there (may be installed already)
-apt-get install -y --no-install-recommends initramfs-tools cloud-initramfs-rooturl busybox-initramfs
+apt-get install -y --no-install-recommends initramfs-tools cloud-initramfs-rooturl cloud-initramfs-copymods busybox-initramfs
 
 sed 's/"$CASPER_GENERATE_UUID"/"always-enable-openssl"/' -i /usr/share/initramfs-tools/hooks/zz-busybox-initramfs
 
@@ -20,4 +20,4 @@ truncate --size=0 /etc/fstab
 apt-get purge -y cloud-initramfs-dyn-netconf cryptsetup-initramfs
 
 # Install kernel (excluding recommended tools like flash-kernel)
-apt-get install -y --no-install-recommends linux-generic-hwe-20.04-edge
+apt-get install -y --no-install-recommends linux-generic-hwe-22.04-edge
